668bd12c9168bca32a98806b32bfdee538cba24a
Mary Joy C. Olivarez <maryjoyolivarez7@gmail.com>
2026-04-15 02:01:46 +0800

    Added Menu and Stock Validation (edited and new array)

--- Product.cs
+++ Product.cs
@@ -1,214 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Numerics;
 using System.Text;
 using System.Transactions;
+using System.Linq;
 
 namespace Olivarez_Mary_Joy_ShoppingCartActivity
-{
+{ 
     internal class Product
     {
         //hii
 
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public int Price { get; set; }
         public int Remaining_Stock { get; set; }
 
-        static void Main(string[] args)
-        {
-            Product product1 = new Product();
-            product1.Id = 1;
-            product1.Name = "Notebook";
-            product1.Price = 75;
-            product1.Remaining_Stock = 100;
-
-            Product product2 = new Product();
-            {
-                product2.Id = 2;
-                product2.Name = "Pencil";
-                product2.Price = 12;
-                product2.Remaining_Stock = 67;
-            }
-            ;
-
-            Product product3 = new Product();
-            {
-                product3.Id = 3;
-                product3.Name = "Bag";
-                product3.Price = 350;
-                product3.Remaining_Stock = 20;
-            };
-
-            while (true)
-            {
-                Console.WriteLine("\n ===Menu System===");
-                Console.WriteLine($"ID: {product1.Id} | Name: {product1.Name} | Price: {product1.Price} | Remaining Stock: {product1.Remaining_Stock}");
-                Console.WriteLine($"ID: {product2.Id} | Name: {product2.Name} | Price: {product2.Price} | Remaining Stock: {product2.Remaining_Stock}");
-                Console.WriteLine($"ID: {product3.Id} | Name: {product3.Name} | Price: {product3.Price} | Remaining Stock: {product3.Remaining_Stock}");
-
-                int number;
-                Console.WriteLine("Enter product number: | Type 0 to finish");
-                if (!int.TryParse(Console.ReadLine(), out number))
-                {
-                    Console.WriteLine("Invalid input! Please enter a number.");
-                    continue;
-                }
-
-                double total = 0;
-                int quantity1 = 0;
-                int quantity2 = 0;
-                int quantity3 = 0; 
-                double change = 0;
-
-                if (number == 1)
-                {
-                    Console.WriteLine($"Adding to Cart Item: {product1.Name} Continue (1) | Type 0 to back");
-                    int cart = Convert.ToInt32(Console.ReadLine());
-
-                    if (cart == 1)
-                    {
-                        Console.WriteLine("\n ===Your cart ===");
-                        Console.WriteLine($"ID: {product1.Id} | Name: {product1.Name} | Price: {product1.Price}");
-
-                        Console.WriteLine("Enter quantity: ");
-                        quantity1 = Convert.ToInt32(Console.ReadLine());
-                        total = product1.Price * quantity1;
-
-                        Console.WriteLine($"Total Price: {total}");
-                        Console.WriteLine("Proceed to pay?: Continue (1) | Type 0 to back");
-                        int pay = Convert.ToInt32(Console.ReadLine());
-
-                        if (pay == 1)
-                        {
-                            Console.WriteLine("Enter amount to Pay: ");
-                            double amount = Convert.ToDouble(Console.ReadLine());
-
-                            if (amount >= product1.Price)
-                            {
-                                change = amount - total;
-                                Console.WriteLine($"Payment Successful! Change: {change}");
-
-                            }
-                            else
-                            {
-                                Console.WriteLine("Insufficient amount, Try again: ");
-                                continue;
-                            }
-                        }
-                        else if (pay == 0)
-                        {
-                            continue;
-                        }
-                    }
-                    else if (cart == 0)
-                    {
-                        continue;
-                    }
-                }
-                else if (number == 2)
-                {
-                    Console.WriteLine($"Adding to Cart Item: {product2.Name} Continue (1) | Type 0 to back");
-                    int cart = Convert.ToInt32(Console.ReadLine());
-
-                    if (cart == 1)
-                    {
-                        Console.WriteLine("\n ===Your cart ===");
-                        Console.WriteLine($"ID: {product2.Id}| Name: {product2.Name} | Price: {product2.Price}");
-
-                        Console.WriteLine("Enter quantity: ");
-                        quantity2 = Convert.ToInt32(Console.ReadLine());
-                        total = product2.Price * quantity2;
-
-                        Console.WriteLine($"Total Price: {total}");
-                        Console.WriteLine("Proceed to pay?: Continue (1) | Type 0 to back");
-                        int pay = Convert.ToInt32(Console.ReadLine());
-
-                        if (pay == 1)
-                        {
-                            Console.WriteLine("Enter amount to Pay: ");
-                            double amount = Convert.ToDouble(Console.ReadLine());
-
-                            if (amount >= total)
-                            {
-                                change = amount - total;
-                                Console.WriteLine($"Payment Successful! Change: {change}");
-                            }
-                            else
-                            {
-                                Console.WriteLine("Insufficient amount, Try again: ");
-                                continue;
-                            }
-                        }
-                        else if (pay == 0)
-                        {
-                            continue;
-                        }
-                    else if (cart == 0)
-                        {
-                            continue;
-                        }
-                    
-                }
-                else if (number == 3)
-                {
-                    Console.WriteLine($"Adding to Cart Item: {product3.Name} Continue (1) | Type 0 to back");
-                    int cart = Convert.ToInt32(Console.ReadLine());
-
-                    if (cart == 1)
-                    {
-                        Console.WriteLine("\n ===Your cart ===");
-                        Console.WriteLine($"ID: {product3.Id}| Name: {product3.Name} | Price: {product3.Price}");
-
-                        Console.WriteLine("Enter quantity: ");
-                        quantity3 = Convert.ToInt32(Console.ReadLine());
-                        total = product3.Price * quantity3;
-
-                        Console.WriteLine($"Total Price: {total}");
-                        Console.WriteLine("Proceed to pay?: Continue (1) | Type 0 to back");
-                        int pay = Convert.ToInt32(Console.ReadLine());
-
-                        if (pay == 1)
-                        {
-                            Console.WriteLine("Enter amount to Pay: ");
-                             double amount = Convert.ToDouble(Console.ReadLine());
-
-                                if (amount >= total) 
-                                {
-                                    change = amount - total;
-                                    Console.WriteLine($"Payment Successful! Change: {change}");
-                                }
-                                else
-                                {
-                                Console.WriteLine("Insufficient amount, Try again: ");
-                                    continue;
-                                }
-                        }
-                        else if (pay == 0)
-                        {
-                                continue;
-                        }
-                    }
-                    else if (cart == 0)
-                    {
-                            continue;
-                    }
-                        
-                
-
-                     
-
-
-
-
-
-
-
-
-
-                    }
-                }
-            }
-        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,49 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+using Olivarez_Mary_Joy_ShoppingCartActivity;
+
+internal class CartItem
+{
+    public string Name { get; set; } = string.Empty;
+    public int Price { get; set; }
+    public int Quantity { get; set; }
+    public double Subtotal { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Product> products = new List<Product>
+            {
+                new Product { Id = 1, Name = "Notebook", Price = 75, Remaining_Stock = 100 },
+                new Product { Id = 2, Name = "Pencil", Price = 12, Remaining_Stock = 100 },
+                new Product { Id = 3, Name = "Bag", Price = 350, Remaining_Stock = 100 },
+                new Product { Id = 4, Name = "Eraser", Price = 10, Remaining_Stock = 100 },
+                new Product { Id = 5, Name = "Ballpen", Price = 18, Remaining_Stock = 100 },
+                new Product { Id = 6, Name = "Stickers", Price = 20, Remaining_Stock = 100 },
+            };
+
+        int cartLimit = 5;
+        List<CartItem> cart = new List<CartItem>();
+
+        while (true)
+        {
+            Console.WriteLine("\n ===Menu System===");
+            foreach (var p in products)
+            {
+                string stockDisplay = "";
+                if (p.Remaining_Stock == 0) stockDisplay = "*** OUT OF STOCK ***";
+                else if (p.Remaining_Stock <= 10) stockDisplay = $"Stock: {p.Remaining_Stock} <<< LOW STOCK";
+                else stockDisplay = $"Stock: {p.Remaining_Stock}";
+
+                Console.WriteLine($"ID: {p.Id} | {p.Name,-10} | Price: {p.Price} | {stockDisplay}");
+            }
+
+            Console.WriteLine("============================");
+
+            
+
+
+
+
+
