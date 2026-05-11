f0f2f429988ab2194621fc3067fd47b2f5fa34b0
Mary Joy C. Olivarez <maryjoyolivarez7@gmail.com>
2026-05-12 01:53:33 +0800

    Getters, setters, and encapsulation principles

--- Product.cs
+++ Product.cs
@@ -11,19 +11,40 @@ namespace Olivarez_Mary_Joy_ShoppingCartActivity
     // Product Class
     public class Product
     {
-        public int Id { get; set; }
-        public string Name { get; set; } = "";
-        public double Price { get; set; }
-        public int RemainingStock { get; set; }
-        public string Category { get; set; } = "";
+        private int Id;
+        private string Name;
+        private double Price;
+        private int RemainingStock;
+        private string category;
+
+
+        // Constructor
+        public Product (int Id, string Name, double Price, int Stock, string Category)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Price = Price;
+            this.RemainingStock = Stock;
+            this.category = Category;
+        }
+
+        // Setters
+        public void SetremainingStock(int stock) { this.RemainingStock = stock; }
+
+        // Getters 
+        public int GetId() { return this.Id; }
+        public string GetName() { return this.Name; }
+
+        public double GetPrice() { return this.Price; }
+        public int GetRemainingStock() { return this.RemainingStock; }
+        public string GetCategory() { return this.category; }
 
         // Display Product 
         public void DisplayProduct() =>
-            Console.WriteLine($"ID: {Id} | {Name,-20} | Price: \u20B1{Price:N2} | Stock: {RemainingStock} | [{Category}]");
+            Console.WriteLine($"ID: {Id} | {Name,-20} | Price: \u20B1{Price:N2} | Stock: {RemainingStock} | [{category}]");
         
-
         // Stock Validations
-        public bool HasEnoughStock(int quantity) => quantity <= RemainingStock;
+        public bool HasEnoughStock(int quantity) => quantity <= RemainingStock; 
 
         // Deduct stock after purchase
         public void DeductStock(int quantity) => RemainingStock -= quantity;
@@ -35,9 +56,24 @@ namespace Olivarez_Mary_Joy_ShoppingCartActivity
     // CartItem Class
     public class CartItem
     {
-        public string Name { get; set; } = "";
-        public double Price { get; set; }
-        public int Quantity { get; set; }
-        public double Subtotal { get; set; }
+        private string Name;
+        private double Price;
+        private int Quantity;
+        private double Subtotal;
+
+        // Setters
+
+        public void SetName(string name) { this.Name = name; }
+        public void SetQuantity (int quantity) { this.Quantity = quantity; }
+        public void SetPrice(double price) { this.Price = price; }
+        public void SetSubtotal(double subtotal) { this.Subtotal = subtotal; }
+
+        // Getters
+
+        public string GetName() { return this.Name; }
+        public int GetQuantity() { return this.Quantity; }
+        public double GetPrice() { return this.Price; }
+        public double GetSubtotaL () { return this.Subtotal; }
+
     }
 } 
